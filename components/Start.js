d26318d852ad217f87bb49bb5b55233b2394d67e
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-11-21 17:26:47 +0100

    defined render method

--- components/Start.js
+++ components/Start.js
@@ -24,3 +24,79 @@ export default class Start extends React.Component {
     // empty string assigned to the state for the user's name and color
     this.state = { name: "", color: "" };
   }
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* image background were added here */}
+        <ImageBackground
+          source={require("../assets/bgImg.png")}
+          style={styles.backgroundImage}
+        >
+          <Text style={styles.title}>Chat App</Text>
+          {/* wrapping the textinput, colors and button here */}
+          <View style={styles.startWrapper}>
+            <TextInput
+              style={styles.input}
+              onChangeText={(name) => this.setState({ name })}
+              value={this.state.name}
+              placeholder="Write your name here..."
+            />
+            <View style={styles.colorWrapper}>
+              <Text style={styles.textColor}>Choose Background Color:</Text>
+              <View style={styles.colors}>
+                <TouchableOpacity
+                  style={[
+                    styles.color,
+                    { backgroundColor: backgroundColors.black },
+                  ]}
+                  onPress={() =>
+                    this.setState({ color: backgroundColors.black })
+                  }
+                />
+                <TouchableOpacity
+                  style={[
+                    styles.color,
+                    { backgroundColor: backgroundColors.purple },
+                  ]}
+                  onPress={() =>
+                    this.setState({ color: backgroundColors.purple })
+                  }
+                />
+                <TouchableOpacity
+                  style={[
+                    styles.color,
+                    { backgroundColor: backgroundColors.grey },
+                  ]}
+                  onPress={() =>
+                    this.setState({ color: backgroundColors.grey })
+                  }
+                />
+                <TouchableOpacity
+                  style={[
+                    styles.color,
+                    { backgroundColor: backgroundColors.green },
+                  ]}
+                  onPress={() =>
+                    this.setState({ color: backgroundColors.green })
+                  }
+                />
+              </View>
+            </View>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() =>
+                this.props.navigation.navigate('Chat', {
+                  name: this.state.name,
+                  color: this.state.color,
+                })
+              }
+            >
+              <Text style={styles.buttonText}>Start Chatting</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </View>
+    );
+  }
+}
+
