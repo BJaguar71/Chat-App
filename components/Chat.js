7a696b8793b84b105e93dcac14d6e8a933851668
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-11-21 17:30:13 +0100

    defined the render method

--- components/Chat.js
+++ components/Chat.js
@@ -7,5 +7,18 @@ export default class Chat extends React.Component {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({title: name})
   }
+  render() {
     // setting the color property for th background color in chat screen
     let color = this.props.route.params.color;
+      return (
+      <View style={[styles.chatContainer, {backgroundColor: color }]}>
+        <TouchableOpacity 
+        onPress={() => this.props.navigation.navigate("Start")} >
+          <Text style={styles.button}>Go to Start</Text>
+        {/* whenever the user presses the button will be redirected to the start screen */}
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
