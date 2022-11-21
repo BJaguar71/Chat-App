f7f28189e2ce7c4e158b224a6eecc22be42a0cab
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-11-21 17:29:20 +0100

    added component's lifecycle to define and show the user's name on the top of the chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -2,3 +2,8 @@ import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 export default class Chat extends React.Component {
+  componentDidMount() {
+    // to show user's name on the top of the chat page
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({title: name})
+  }
