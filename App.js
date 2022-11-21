b892d71f195d0dd26abb14b95cfe0f1b9c1f2fc2
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-11-21 17:19:55 +0100

    imported react Navigation

--- App.js
+++ App.js
@@ -3,3 +3,7 @@ import React, { Component } from 'react';
 // import react native gesture handler
 import 'react-native-gesture-handler';
 
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
