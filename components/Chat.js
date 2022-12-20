d0902c11fda43226dede1323db938ed6c05ec822
BJaguar71 <87417713+BJaguar71@users.noreply.github.com>
2022-12-20 18:24:26 +0100

    added GiftedChat element and wrapped it inside a View element

--- components/Chat.js
+++ components/Chat.js
@@ -72,13 +72,9 @@ export default class Chat extends React.Component {
     // setting the color property for th background color in chat screen
     let color = this.props.route.params.color;
     return (
-      <View style={[styles.chatContainer, { backgroundColor: color }]}>
-        <TouchableOpacity
-          onPress={() => this.props.navigation.navigate("Start")}
-        >
-          <Text style={styles.button}>Go to Start</Text>
-          {/* whenever the user presses the button will be redirected to the start screen */}
-        </TouchableOpacity>
+      <View style={{ flex: 1, backgroundColor: color }}>
+        <GiftedChat
+        />
       </View>
     );
   }
