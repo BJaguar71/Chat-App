7484cf6894ce306add967fe0916bb8acae9a6971
Bjaguar71 <mrym.hanifi@gmail.com>
2023-01-06 20:04:46 +0100

    defined custom map view

--- components/Chat.js
+++ components/Chat.js
@@ -266,6 +266,24 @@ export default class Chat extends React.Component {
     return <CustomActions {...props} />;
   };
 
+  // custom mapview
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
   render() {
     // to show user's name on the top of the chat page
     let name = this.props.route.params.name;
